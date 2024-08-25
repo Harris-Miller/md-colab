c2d901ac91c84155437308ce61840f34ae0244e6
harris-miller <harrismillerconsulting@gmail.com>
2024-08-25 15:21:22 -0600

    crtd testing with yjs

---
+++ server/src/crtd-test.ts
@@ -0,0 +1,10 @@
+import * as Y from 'yjs';
+
+const doc = new Y.Doc();
+
+const bodyText = doc.getText('body');
+
+bodyText.insert(0, 'World', { bold: true });
+bodyText.insert(0, 'Hello ');
+
+console.log(bodyText.toDelta());
